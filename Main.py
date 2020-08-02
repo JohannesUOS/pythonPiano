3bc523dcb0badb27486797fc039ce4546f6b4140
JohannesUOS <jhaefner@uni-osnabrueck.de>
2020-08-03 00:15:37 +0200

    ADDED piano sample files

--- Main.py
+++ Main.py
@@ -1,3 +1,14 @@
-#Hello World Test
-print("Hello World")
-print("This shit does not work")
+#General first test
+import simpleaudio as sa
+
+#import file as wave object
+wave_obj = sa.WaveObject.from_wave_file("piano_samples/1A.wav")
+
+#starting playback
+play_obj = wave_obj.play()
+
+#waiting for the playback to finish before exiting script
+play_obj.wait_done()
+
+print("FINISHED PLAYBACK")
+
